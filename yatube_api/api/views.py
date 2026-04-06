56b57a29a1dca66c47dc68701227443e3ae4b05e
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:27:44 +0700

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets, filters, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from posts.models import Post, Group, Comment, Follow
 from .serializers import (
     PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
@@ -47,3 +48,17 @@ class FollowViewSet(mixins.CreateModelMixin,
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+            serializer.data,
+            status=201,
+            headers=headers
+        )
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
