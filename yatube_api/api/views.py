0016082df5043d9e5c7f332d6250ddad265bd1e6
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 13:50:47 +0700

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -46,6 +46,3 @@ class FollowViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        serializer.save(user=self.request.user)
