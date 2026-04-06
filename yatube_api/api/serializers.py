d756f0be24d080241b4c8a42fe3ccba8b7908b59
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:43:21 +0700

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -45,4 +45,10 @@ class FollowSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 'Нельзя подписаться на самого себя'
             )
+        if Follow.objects.filter(
+            user=request.user, following=value
+        ).exists():
+            raise serializers.ValidationError(
+                'Вы уже подписаны на этого пользователя'
+            )
         return value
