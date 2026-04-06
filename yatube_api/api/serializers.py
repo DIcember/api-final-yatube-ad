4e93ab7cc5eb3ffe4fdc3b9b8cd92105be337813
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 13:02:04 +0700

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,7 +4,24 @@ from rest_framework.relations import SlugRelatedField
 
 from posts.models import Comment, Post
 
-
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    following = serializers.StringRelatedField()
+    
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+    
+    def validate_following(self, value):
+        request = self.context.get('request')
+        if request.user == value:
+            raise serializers.ValidationError('Нельзя подписаться на самого себя')
+        return value
+    
+    def create(self, validated_data):
+        validated_data['user'] = self.context.get('request').user
+        return super().create(validated_data)
+        
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
