d79c6cc0109ec42a68c0e3e17ca4b773dd890d02
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:43:56 +0700

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -17,16 +17,28 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+    # НЕ ПИШИТЕ МЕТОД list() - он уже есть в ModelViewSet
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    # НЕ ПИШИТЕ МЕТОД list() - он уже есть
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        return Comment.objects.filter(post_id=post_id)
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        post = Post.objects.get(id=post_id)
+        serializer.save(author=self.request.user, post=post)
+    # НЕ ПИШИТЕ МЕТОД list() - он уже есть
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
