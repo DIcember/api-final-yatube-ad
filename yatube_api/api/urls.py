cecc1815b490af2f7e97bbcc02de37cebbbc8421
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 13:44:53 +0700

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -13,8 +13,7 @@ router.register(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('api.urls')),
-    path('api/v1/auth/', include('djoser.urls.jwt')),  # JWT эндпоинты
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
 ]
