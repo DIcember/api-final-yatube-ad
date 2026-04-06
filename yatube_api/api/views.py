336832492aafffa2e8057619474a1414cfc60085
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:44:08 +0700

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, filters
-from rest_framework import mixins
+from rest_framework import viewsets, filters, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
 from posts.models import Post, Group, Comment, Follow
@@ -17,28 +16,16 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-    # НЕ ПИШИТЕ МЕТОД list() - он уже есть в ModelViewSet
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    # НЕ ПИШИТЕ МЕТОД list() - он уже есть
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     permission_classes = (IsAuthorOrReadOnly,)
-
-    def get_queryset(self):
-        post_id = self.kwargs.get('post_id')
-        return Comment.objects.filter(post_id=post_id)
-
-    def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(id=post_id)
-        serializer.save(author=self.request.user, post=post)
-    # НЕ ПИШИТЕ МЕТОД list() - он уже есть
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
