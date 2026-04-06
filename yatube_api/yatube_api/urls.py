e0c7711216cbb99bedfd138e16b5a7e25d77e172
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:42:27 +0700

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -17,6 +17,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('api/v1/auth/', include('djoser.urls')),
     path('api/v1/auth/', include('djoser.urls.jwt')),
     path(
         'redoc/',
