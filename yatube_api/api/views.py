2e31651ad1a2242b42e0293a8d9e5b8b1a15f64b
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 13:47:51 +0700

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,9 +1,7 @@
 from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.pagination import LimitOffsetPagination
-from django_filters.rest_framework import (  # Разбиваем длинный импорт
-    DjangoFilterBackend
-)
+from django_filters.rest_framework import DjangoFilterBackend
 from posts.models import Post, Group, Comment, Follow
 from .serializers import (
     PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
@@ -44,7 +42,7 @@ class FollowViewSet(viewsets.ModelViewSet):
     serializer_class = FollowSerializer
     permission_classes = (IsAuthenticated,)
     filter_backends = (filters.SearchFilter, DjangoFilterBackend)
-    search_fields = ('following__username', 'user__username')
+    search_fields = ('following__username',)
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
