92dfce882c56a913b16a34b4e886a4175f485b27
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 13:26:47 +0700

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -12,7 +12,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
-    group = models.ForeignKey(  # ЭТО ПОЛЕ ОБЯЗАТЕЛЬНО ДОЛЖНО БЫТЬ!
+    group = models.ForeignKey(
         'Group',
         on_delete=models.SET_NULL,
         blank=True,
