7f55eac0de4edad8037ffa161c22c878f47df2d2
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:17:01 +0700

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -61,9 +61,9 @@ class Follow(models.Model):
     )
 
     class Meta:
-        unique_together = ('user', 'following')
-        verbose_name = 'Подписка'
-        verbose_name_plural = 'Подписки'
-
-    def __str__(self):
-        return f'{self.user} подписан на {self.following}'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_follow'
+            )
+        ]
