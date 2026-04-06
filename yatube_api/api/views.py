b89d26e557d99b42be272890329d269358f602d7
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:56:58 +0700

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,49 +1,48 @@
-from rest_framework import viewsets, filters, mixins
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import LimitOffsetPagination
-from posts.models import Post, Group, Comment, Follow
+from rest_framework import viewsets, permissions, filters
+from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
+from posts.models import Post, Comment, Group, Follow
 from .serializers import (
-    PostSerializer, GroupSerializer, CommentSerializer, FollowSerializer
+    PostSerializer, CommentSerializer, 
+    GroupSerializer, FollowSerializer
 )
 from .permissions import IsAuthorOrReadOnly
 
+class CustomPagination(PageNumberPagination):
+    """Кастомная пагинация для возврата списка"""
+    page_size = 10
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
-    pagination_class = LimitOffsetPagination
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    pagination_class = CustomPagination
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-
-class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-
-
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def get_queryset(self):
         post_id = self.kwargs.get('post_id')
         return Comment.objects.filter(post_id=post_id)
 
     def perform_create(self, serializer):
-        post_id = self.kwargs.get('post_id')
-        post = Post.objects.get(id=post_id)
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
 
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    # ← Важно: для групп НЕТ пагинации по ТЗ, но должен возвращаться список!
 
-class FollowViewSet(mixins.CreateModelMixin,
-                    mixins.ListModelMixin,
-                    viewsets.GenericViewSet):
+class FollowViewSet(viewsets.ModelViewSet):
     serializer_class = FollowSerializer
-    permission_classes = (IsAuthenticated,)
-    filter_backends = (filters.SearchFilter,)
-    search_fields = ('following__username',)
+    permission_classes = [permissions.IsAuthenticated]  # ← только для авторизованных!
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['following__username']
 
     def get_queryset(self):
         return Follow.objects.filter(user=self.request.user)
