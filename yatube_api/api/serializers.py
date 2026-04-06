74945d009a960a4be8a9a3c57d02badbfdaa304e
DIcember <163012780+DIcember@users.noreply.github.com>
2026-04-06 14:56:31 +0700

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,35 +1,43 @@
 from rest_framework import serializers
-from posts.models import Post, Group, Comment, Follow
+from posts.models import Post, Comment, Group, Follow
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'group')
+        fields = '__all__'
+        read_only_fields = ('author', 'pub_date')
 
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
+        read_only_fields = ('author', 'post')
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
-
-
-class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField(read_only=True)
-
-    class Meta:
-        model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
-        read_only_fields = ('post',)
-
+        fields = '__all__'
 
 class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.StringRelatedField(read_only=True)
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
     following = serializers.SlugRelatedField(
         slug_field='username',
         queryset=User.objects.all()
@@ -37,18 +45,20 @@ class FollowSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Follow
-        fields = ('user', 'following')
+        fields = '__all__'
+        validators = []  # ← важно для кастомной валидации
 
-    def validate_following(self, value):
+    def validate(self, data):
         request = self.context.get('request')
-        if request.user == value:
+        if request.user == data['following']:
             raise serializers.ValidationError(
-                'Нельзя подписаться на самого себя'
+                'Нельзя подписаться на самого себя!'
             )
         if Follow.objects.filter(
-            user=request.user, following=value
+            user=request.user,
+            following=data['following']
         ).exists():
             raise serializers.ValidationError(
                 'Вы уже подписаны на этого пользователя'
             )
-        return value
+        return data
